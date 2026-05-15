f954a4f1712f13b934448237e233aaed866ac9db
yeon17s <yeons7j@gmail.com>
2026-05-15 10:47:28 +0900

    chore: Swagger 설정 추가
    - springdoc-openapi 의존성 추가
    - SwaggerConfig 빈(Bean) 등록
    - API 타이틀, 설명, 버전 정보 설정

--- src/main/java/com/ecommerce/ecommerce/config/SwaggerConfig.java
+++ src/main/java/com/ecommerce/ecommerce/config/SwaggerConfig.java
@@ -1,4 +1,18 @@
 package com.ecommerce.ecommerce.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("E-commerce API")
+                        .description("E-commerce 서비스 API 명세서")
+                        .version("v1.0"));
+    }
 }
